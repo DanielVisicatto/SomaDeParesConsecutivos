a12ba96cf5da0b568da80af1ef62553043ec7dce
Rozicleide <roziibr@hotmail.com>
2022-02-20 11:35:06 -0400

    Add project files.

---
+++ SomaDeParesConsecutivos/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace SomaDeParesConsecutivos
+
+    {
+
+        internal static class Program
+
+        {
+
+            private static void Main()
+
+            {
+
+                int n;
+
+                while ((n = int.Parse(Console.ReadLine())) != 0)
+
+                {
+
+                    int soma = 0;
+
+
+
+                    if (n % 2 != 0)
+
+                    {
+
+                        n++;
+
+                    }
+
+
+
+                    for (int i = 0; i < 5; i++)
+
+                    {
+
+                        soma += n + i * 2;
+
+                    }
+
+
+
+                    Console.WriteLine(soma);
+
+                }
+
+            }
+
+        }
+
+    }
+
+    
+
